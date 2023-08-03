24bf357f9c804bff2f034a87cba9716cef0412c9
CodeNil <codenil@proton.me>
2023-08-03 04:14:24 +0100

    Much cleaner code

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,28 @@
 #![allow(non_snake_case)]
-use bincode::{config, Decode, Encode};
+use atom_syndication::Feed;
 use dioxus::prelude::*;
-use feed_rs::parser;
+use serde::{Deserialize, Serialize};
 use std::io::Cursor;
 
-#[derive(Decode, Encode, PartialEq, Clone)]
+#[derive(Deserialize, Serialize, PartialEq, Clone)]
 pub enum ReadStatus {
     Fresh,
     Saved,
     Archived,
 }
 
-#[derive(Decode, Encode, PartialEq, Clone)]
-pub enum Category {
-    Entertainment,
-    Tech,
-    CodeUpdates,
-    GameDev,
-    Politics,
-    Business,
-    Science,
-    Health,
-    Sports,
-    Other,
-}
-
-impl Category {
-    #[must_use]
-    pub fn description(&self) -> &str {
-        match *self {
-            Category::Entertainment => "Movies, music, and entertainment",
-            Category::Tech => "Tech articles, gadget reviews, IT news",
-            Category::CodeUpdates => "Updates from coding, software projects",
-            Category::GameDev => "Game development insights, updates",
-            Category::Politics => "Political analysis, government, elections",
-            Category::Business => "Business insights, market trends, economic analyses",
-            Category::Science => "Discoveries, research, science breakthroughs",
-            Category::Health => "Health advice, medical research, nutrition",
-            Category::Sports => "Sports updates, scores, news",
-            Category::Other => "Content that doesn't fit other categories",
-        }
-    }
-}
-
-#[derive(Decode, Encode, PartialEq, Clone)]
-pub enum ArticleType {
-    Article,
-    Video,
-}
-
-#[derive(Decode, Encode, PartialEq, Clone)]
-struct Article {
+#[derive(Deserialize, Serialize, PartialEq, Clone)]
+struct Channel {
     link: String,
     title: String,
-    category: Category,
-    article_type: ArticleType,
+    icon: String,
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Clone)]
+struct Article {
+    link: String,
+    channel: Channel,
+    title: String,
     published: String,
     image: String,
     summary: String,
@@ -66,14 +34,8 @@ pub fn App(cx: Scope) -> Element {
     use_shared_state_provider(cx, Vec::<Article>::new);
 
     cx.render(rsx! {
-        div {
-            display: "flex",
-            flex_direction: "row",
-            width: "100%",
-            div {
-                width: "100%",
-                Articles {}
-            }
+        div { display: "flex", flex_direction: "row", width: "100%",
+            div { width: "100%", Articles {} }
         }
     })
 }
@@ -99,47 +61,26 @@ fn ArticleListing(cx: Scope, article: Article) -> Element {
     let Article {
         title,
         link,
-        category,
         published,
-        summary,
         ..
     } = article;
+    let icon = &article.channel.icon;
 
     cx.render(rsx! {
-        div {
-            padding: "0.5rem",
-            position: "relative",
-            div {
-                font_size: "1.5rem",
-                a {
-                    href: link.as_str(),
-                    "{title}"
-                }
-            }
-            div {
-                display: "flex",
-                flex_direction: "row",
-                color: "gray",
-                div {
-                    "{category.description()}"
-                }
-                div {
-                    padding_left: "0.5rem",
-                    "{published}"
-                }
-            }
-            div {
-                "{summary}"
+        div { padding: "0.5rem", position: "relative",
+            div { display: "flex", flex_direction: "row", color: "gray",
+                div { font_size: "1.5rem", a { href: link.as_str(), "{title}" } }
+                div { padding_left: "0.5rem", "{published}" }
+                img { width: "1.5rem", height: "1.5rem", src: icon.as_str() }
             }
         }
     })
 }
 
 async fn get_articles() -> Result<Vec<Article>, reqwest::Error> {
-    let rss_sources = vec![
-        "https://www.theverge.com/rss/index.xml",
-        "http://feeds.bbci.co.uk/news/technology/rss.xml",
-    ];
+    let rss_sources = vec!["https://www.theverge.com/rss/index.xml"];
+    // http://feeds.bbci.co.uk/news/technology/rss.xml
+    // https://hnrss.org/frontpage
     // let db = sled::open("database").expect("Failed to open the database");
     let mut articles = Vec::<Article>::new();
 
@@ -173,25 +114,25 @@ async fn process_source(source: &str) -> Result<Vec<Article>, Box<dyn std::error
     let response = reqwest::get(source).await?;
     let bytes = response.bytes().await?;
     let cursor = Cursor::new(bytes);
-    let feed = parser::parse(cursor)?;
+    let feed = Feed::read_from(cursor).unwrap();
 
     let mut articles = Vec::new();
 
-    for entry in feed.entries {
-        // If entry doesnt have title skip it
-        if entry.title.is_none() {
-            continue;
-        }
-        let entry_title = entry.title.unwrap().content;
-        let entry_summary = entry.summary.map_or_else(String::new, |s| s.content);
+    let channel = Channel {
+        link: feed.id,
+        title: feed.title.value,
+        icon: feed.icon.unwrap_or_default(),
+    };
 
+    for entry in feed.entries {
+        let entry_title = entry.title.value;
+        let entry_summary = entry.summary.map_or_else(String::new, |s| s.value);
         let entry_published = entry.published.unwrap_or_default();
 
         let article = Article {
             link: entry.id,
+            channel: channel.clone(),
             title: entry_title,
-            category: Category::Tech,
-            article_type: ArticleType::Article,
             published: entry_published.to_string(),
             image: String::new(),
             summary: entry_summary,
