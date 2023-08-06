623c58aad85afbb8461f63f2ac20848c8d2664d2
CodeNil <codenil@proton.me>
2023-08-06 06:44:55 +0100

    Cleaned main function

--- src/main.rs
+++ src/main.rs
@@ -20,25 +20,34 @@ use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
+    // Initialize tracing
     tracing_subscriber::fmt::init();
 
-    let db: Arc<Db> = Arc::new(sled::open("database").expect("Failed to open database"));
-    let db_clone1 = db.clone();
-    let db_clone2 = db.clone();
+    // Database setup
+    let db = Arc::new(sled::open("database").expect("Failed to open database"));
 
+    // Create clones for the router
+    let db_for_get = db.clone();
+    let db_for_put = db.clone();
+
+    // Router setup
     let app = Router::new()
         .nest_service("/", ServeDir::new("assets"))
-        .route("/articles", get(move || articles::get_articles(db_clone1)))
+        .route("/articles", get(move || articles::get_articles(db_for_get)))
         .route(
             "/articles/:link/:new_status",
-            put(|path: Path<(String, String)>| articles::update_article_status(path, db_clone2)),
+            put(move |path: Path<(String, String)>| {
+                articles::update_article_status(path, db_for_put)
+            }),
         )
         .layer(ServiceBuilder::new().layer(CompressionLayer::new()));
 
+    // Server setup
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on {addr}");
     let server = axum::Server::bind(&addr).serve(app.into_make_service());
 
+    // Article puller setup
     let article_puller = async {
         let mut interval = interval(Duration::from_secs(20 * 60));
         loop {
@@ -49,6 +58,7 @@ async fn main() {
         }
     };
 
+    // Run server and article puller concurrently
     tokio::select! {
         _ = server => {
             eprintln!("Server exited.");
@@ -66,39 +76,36 @@ struct Config {
 
 /// Get articles and write them to the database
 async fn pull_articles(db: Arc<Db>) {
-    // Read and parse the TOML file, grab needed info
-    let contents = read_to_string("feeds.toml").expect("Something went wrong reading the file");
-    let mut config: Config = toml::from_str(&contents).expect("Error parsing the TOML");
+    let contents = read_to_string("feeds.toml").expect("Failed to read the file");
+    let mut config: Config = toml::from_str(&contents).expect("Failed to parse the TOML");
     let mut config_changed = false;
     let mut new_rss = Vec::new();
+
     for feed in &config.rss {
-        // If any of feeds variables are Option<None> then get new data
         let needs_fresh =
             feed.title.is_none() || feed.icon.is_none() || feed.dominant_color.is_none();
-        let new_data = match channel::get_channel_data(&db.clone(), needs_fresh, feed).await {
-            Ok(channel_data) => channel_data,
+
+        match channel::get_channel_data(&db, needs_fresh, feed).await {
+            Ok(channel_data) => {
+                new_rss.push(channel_data);
+                config_changed = true;
+            }
             Err(e) => {
                 eprintln!("Error getting channel data for {}: {}", feed.rss_url, e);
                 new_rss.push(feed.clone());
-                continue;
             }
-        };
-        // Overwrite the feed in config with new_data
-        new_rss.push(new_data.clone());
-        config_changed = true;
-    }
-    // Write config if anything has changed
-    if config_changed {
-        config.rss = new_rss;
-        let toml = toml::to_string(&config).expect("Error serializing the TOML");
-        write("feeds.toml", toml).expect("Error writing to file");
+        }
     }
 
-    // Pull the articles data
+    if config_changed {
+        config.rss = new_rss;
+        let toml = toml::to_string(&config).expect("Failed to serialize the TOML");
+        write("feeds.toml", toml).expect("Failed to write to file");
+    }
+
     for source in config.rss {
-        match articles::process_source(&source.rss_url, db.clone()).await {
-            Ok(_) => {}
-            Err(e) => eprintln!("Error processing source {}: {}", source.rss_url, e),
+        if let Err(e) = articles::process_source(&source.rss_url, db.clone()).await {
+            eprintln!("Error processing source {}: {}", source.rss_url, e);
         }
     }
 }
